d63c830deceec6292365942909e00fffb00f18d3
rahulbyte6789 <75127102+rahulbyte6789@users.noreply.github.com>
2022-02-19 18:17:08 +0530

    Add files via upload

---
+++ Q1 Controllers/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNetExam.Controllers
+{
+    public class Class1
+    {
+        public int ProductId { get; internal set; }
+        public string ProductName { get; internal set; }
+        public int Rate { get; internal set; }
+        public string Description { get; internal set; }
+        public string CategoryName { get; internal set; }
+    }
+}
---
+++ Q1 Models/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DotNetExam.Models
+{
+    public class Class1
+    {
+        [Key]
+        public int ProductId { get; set; }
+        
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+        
+        public decimal Rate { get; set; }
+
+        public string Description { get; set; }
+
+        public string CategoryName { get; set; }
+
+    }
+}
